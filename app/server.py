a8d9e118dc0c10b6905f16549994cd11c0217de4
richardjlv <richardjlv@users.noreply.github.com>
2019-11-20 14:29:38 -0300

    CSS modificado

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = '<Your file url here>'
+export_file_name = '<Your filename here>'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['<Your classes here>']
 path = Path(__file__).parent
 
 app = Starlette()
