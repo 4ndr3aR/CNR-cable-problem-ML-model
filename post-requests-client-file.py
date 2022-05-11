e9aa34c4dca8cfd10cc0e6b578b4e6092f28303d
Andrea Ranieri <andrea.ranieri@cnr.it>
2022-05-11 16:05:06 +0200

    Added username and password capabilities to post-requests-client-file.py...

--- post-requests-client-file.py
+++ post-requests-client-file.py
@@ -9,6 +9,12 @@ if len(sys.argv) < 2:
 if len(sys.argv) < 3:
 	print(f'Please provide a port number...')
 	sys.exit()
+if len(sys.argv) >= 4:
+	username = sys.argv[3]
+	print(f'Using username: {username}')
+if len(sys.argv) >= 5:
+	password = sys.argv[4]
+	print(f'Using password: {password}')
 
 kfname = sys.argv[1]
 print(f'Reading filename: {kfname}')
@@ -21,7 +27,11 @@ print(f'{kfdata = }')
 #port = 55563
 port = int(sys.argv[2])
 print(f'Performing post request on port {port}\n')
-r = requests.post(f'http://deeplearning.ge.imati.cnr.it:{port}/post', data={'filename': kfname, 'kistlerfile': kfdata})
+
+if len(sys.argv) < 5:
+	r = requests.post(f'http://deeplearning.ge.imati.cnr.it:{port}/post', data={'filename': kfname, 'kistlerfile': kfdata})
+else:
+	r = requests.post(f'http://deeplearning.ge.imati.cnr.it:{port}/post', data={'filename': kfname, 'kistlerfile': kfdata, 'username': username, 'password': password})
 
 # And done.
 print(f'Answer:')
